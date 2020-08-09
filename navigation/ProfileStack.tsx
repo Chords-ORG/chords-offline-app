625c98c6c7e667eb0cc180e15d239c3532667c6c
Gustavo Marques <gustavomarques.gml@gmail.com>
2020-08-09 17:45:01 -0300

    Configuration of top profile header

--- navigation/ProfileStack.tsx
+++ navigation/ProfileStack.tsx
@@ -6,7 +6,7 @@ import { ProfileStackParamList, ProfileTabsParamList } from '../types';
 import { createStackNavigator } from '@react-navigation/stack';
 import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
 import SampleScreen from '../screens/SampleScreen';
-import { StyleSheet, Text, View, Image } from 'react-native';
+import { StyleSheet, Text, View, Image, TouchableOpacity } from 'react-native';
 
 const Stack = createStackNavigator<ProfileStackParamList>();
 
@@ -16,7 +16,7 @@ export default function ProfileStack() {
             <Stack.Screen
                 name="ProfileTabs"
                 component={ProfileTabs}
-                options={{ headerTitle: 'MyProfile' }}
+                options={{ header: () => profileHeader(false) }}
             />
             <Stack.Screen
                 name="MyList"
@@ -76,77 +76,172 @@ const Tab = createMaterialTopTabNavigator<ProfileTabsParamList>();
 
 function ProfileTabs() {
     return (
-        <Tab.Navigator 
-            tabBarOptions={{ 
-                activeTintColor: '#333333', 
-                inactiveTintColor:'#828282',
-                indicatorStyle :{
-                    backgroundColor:'#333333'
-              }
+        <Tab.Navigator
+            tabBarOptions={{
+                activeTintColor: '#333333',
+                inactiveTintColor: '#828282',
+                indicatorStyle: {
+                    backgroundColor: '#333333'
+                }
             }}
-            
+
         >
             <Tab.Screen
                 name="Chords"
                 component={SampleScreen}
                 options={{
-                    tabBarLabel: ({focused}) => chordsIcon(focused)
+                    tabBarLabel: ({ focused }) => chordsIcon(focused)
                 }}
             />
             <Tab.Screen
                 name="Lists"
                 component={SampleScreen}
                 options={{
-                    tabBarLabel: ({focused}) => listsIcon(focused)
+                    tabBarLabel: ({ focused }) => listsIcon(focused)
                 }}
             />
             <Tab.Screen
                 name="MoreOptions"
                 component={SampleScreen}
                 options={{
-                    tabBarLabel: ({focused}) => moreIcon(focused)
+                    tabBarLabel: ({ focused }) => moreIcon(focused)
                 }}
             />
         </Tab.Navigator>
     );
 }
 
-function chordsIcon(focus:boolean){
+function profileHeader(logged: boolean) {
+    if (logged)
+        return (
+            <View style={styles.header}>
+                <View style={styles.left}>
+                    <Image
+                        style={styles.avatar}
+                        source={require('../assets/images/sample_avatar.png')}
+                    />
+                    <View style={styles.name_container}>
+                        <Text style={styles.h1}> Fulano da silva </Text>
+                        <Text style={styles.h2}> @fulano </Text>
+                    </View>
+                </View>
+                <View style={styles.right}>
+                    <TouchableOpacity style={styles.button}>
+                        <Text style={styles.button_text}> Editar perfil </Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity style={[styles.button, { backgroundColor: '#F2F2F2' }]}>
+                        <Text style={[styles.button_text, { color: '#333333' }]}> Sair </Text>
+                    </TouchableOpacity>
+                </View>
+            </View>
+        );
+    else
+        return (
+            <View style={styles.header}>
+                <View style={styles.left}>
+                    <Image
+                        style={styles.avatar}
+                        source={require('../assets/images/sample_avatar.png')}
+                    />
+                    <View style={styles.name_container}>
+                        <Text style={[styles.h1, { textAlign: 'center' }]}> Você ainda não está cadastrado </Text>
+                    </View>
+                </View>
+                <View style={styles.right}>
+                    <TouchableOpacity style={styles.button}>
+                        <Text style={styles.button_text}> Entrar </Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity style={[styles.button, { backgroundColor: '#F2F2F2' }]}>
+                        <Text style={[styles.button_text, { color: '#333333' }]}> Criar Conta </Text>
+                    </TouchableOpacity>
+                </View>
+            </View>
+        );
+}
+function chordsIcon(focus: boolean) {
     const focused = require('../assets/images/chord_icon.png')
     const unfocused = require('../assets/images/chord_icon_gray.png')
-    return(
+    return (
         <Image
             style={styles.icon}
-            source={ focus ? focused : unfocused }
+            source={focus ? focused : unfocused}
         />
     )
 }
 
-function listsIcon(focus:boolean){
+function listsIcon(focus: boolean) {
     const focused = require('../assets/images/list_icon.png')
     const unfocused = require('../assets/images/list_icon_gray.png')
-    return(
+    return (
         <Image
             style={styles.icon}
-            source={ focus ? focused : unfocused }
+            source={focus ? focused : unfocused}
         />
     )
 }
 
-function moreIcon(focus:boolean){
+function moreIcon(focus: boolean) {
     const focused = require('../assets/images/more_icon.png')
     const unfocused = require('../assets/images/more_icon_gray.png')
-    return(
+    return (
         <Image
             style={styles.icon}
-            source={ focus ? focused : unfocused }
+            source={focus ? focused : unfocused}
         />
     )
 }
 const styles = StyleSheet.create({
-    icon:{
-        height:20,
-        width:20,
-    }
+    icon: {
+        height: 20,
+        width: 20,
+    },
+    header: {
+        backgroundColor: '#fff',
+        height: 150,
+        width: '100%',
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-between',
+        padding: 20,
+        paddingTop: 60,
+    },
+    left: {
+        flexDirection: 'row'
+    },
+    right: {
 
+    },
+    avatar: {
+        height: 50,
+        width: 50,
+    },
+    h1: {
+        fontSize: 16,
+        color: '#333333',
+        fontFamily: 'roboto-bold',
+    },
+    h2: {
+        fontSize: 12,
+        color: '#828282',
+        fontFamily: 'roboto',
+    },
+    name_container: {
+        marginLeft: 10,
+        width: 150,
+        justifyContent:'center'
+    },
+    button: {
+        width: 100,
+        height: 30,
+        backgroundColor: '#2F80ED',
+        borderRadius: 5,
+        alignItems: 'center',
+        justifyContent: 'center',
+        marginBottom: 10
+    },
+    button_text: {
+        fontSize: 12,
+        fontFamily: 'roboto-bold',
+        color: '#F2F2F2'
+    }
 })
