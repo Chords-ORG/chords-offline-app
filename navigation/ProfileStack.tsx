5252fe08d915c3f24c48aeadcb72372ee74c00da
Gustavo Marques <gustavomarques.gml@gmail.com>
2020-08-09 17:26:10 -0300

    Configuration of top tabs icons

--- navigation/ProfileStack.tsx
+++ navigation/ProfileStack.tsx
@@ -6,6 +6,7 @@ import { ProfileStackParamList, ProfileTabsParamList } from '../types';
 import { createStackNavigator } from '@react-navigation/stack';
 import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
 import SampleScreen from '../screens/SampleScreen';
+import { StyleSheet, Text, View, Image } from 'react-native';
 
 const Stack = createStackNavigator<ProfileStackParamList>();
 
@@ -75,20 +76,77 @@ const Tab = createMaterialTopTabNavigator<ProfileTabsParamList>();
 
 function ProfileTabs() {
     return (
-      <Tab.Navigator>
-        <Tab.Screen 
-            name="Chords" 
-            component={SampleScreen} 
-        />
-        <Tab.Screen 
-            name="Lists" 
-            component={SampleScreen} 
-        />
-        <Tab.Screen 
-            name="MoreOptions" 
-            component={SampleScreen} 
-        />
-      </Tab.Navigator>
+        <Tab.Navigator 
+            tabBarOptions={{ 
+                activeTintColor: '#333333', 
+                inactiveTintColor:'#828282',
+                indicatorStyle :{
+                    backgroundColor:'#333333'
+              }
+            }}
+            
+        >
+            <Tab.Screen
+                name="Chords"
+                component={SampleScreen}
+                options={{
+                    tabBarLabel: ({focused}) => chordsIcon(focused)
+                }}
+            />
+            <Tab.Screen
+                name="Lists"
+                component={SampleScreen}
+                options={{
+                    tabBarLabel: ({focused}) => listsIcon(focused)
+                }}
+            />
+            <Tab.Screen
+                name="MoreOptions"
+                component={SampleScreen}
+                options={{
+                    tabBarLabel: ({focused}) => moreIcon(focused)
+                }}
+            />
+        </Tab.Navigator>
     );
-  }
-  
+}
+
+function chordsIcon(focus:boolean){
+    const focused = require('../assets/images/chord_icon.png')
+    const unfocused = require('../assets/images/chord_icon_gray.png')
+    return(
+        <Image
+            style={styles.icon}
+            source={ focus ? focused : unfocused }
+        />
+    )
+}
+
+function listsIcon(focus:boolean){
+    const focused = require('../assets/images/list_icon.png')
+    const unfocused = require('../assets/images/list_icon_gray.png')
+    return(
+        <Image
+            style={styles.icon}
+            source={ focus ? focused : unfocused }
+        />
+    )
+}
+
+function moreIcon(focus:boolean){
+    const focused = require('../assets/images/more_icon.png')
+    const unfocused = require('../assets/images/more_icon_gray.png')
+    return(
+        <Image
+            style={styles.icon}
+            source={ focus ? focused : unfocused }
+        />
+    )
+}
+const styles = StyleSheet.create({
+    icon:{
+        height:20,
+        width:20,
+    }
+
+})
