f4f05c110828b2e7c59676f15118ca5f899f97c4
Gustavo Marques <gustavomarques.gml@gmail.com>
2020-08-25 14:57:15 -0300

    Refact of Artist screen

--- screens/ArtistScreen.tsx
+++ screens/ArtistScreen.tsx
@@ -14,35 +14,33 @@ import { RootStackParamList, ArtistType, MusicType, SearchStackParamList } from 
 import { StackScreenProps } from '@react-navigation/stack';
 import { get_artist, get_artist_musics } from '../functions/requests'
 import { ScrollView } from 'react-native-gesture-handler';
+import { light_style, dark_style } from '../constants/Styles'
 
 export default function ArtistScreen({ navigation, route }: StackScreenProps<RootStackParamList, 'ArtistScreen'>) {
-
+  const load_data = async (artist_id: number) => {
+    setLoading(true);
+    const artist = await get_artist(artist_id);
+    const musics = await get_artist_musics(artist_id);
+    setArtist(artist);
+    setMusics(musics);
+    setLoading(false);
+  };
   useEffect(() => {
     const unsubscribe = navigation.addListener('focus', () => {
-      setLoading(true);
-      get_artist(route.params.artist_id).then((artist) => {
-        setArtist(artist);
-      }).catch(error => {
-        setLoading(false);
-        Alert.alert(error.title, error.message)
-      })
-      get_artist_musics(route.params.artist_id).then((musics) => {
-        setMusics(musics);
-        setLoading(false);
-      }).catch(error => {
+      load_data(route.params.artist_id).catch(error => {
         setLoading(false);
         Alert.alert(error.title, error.message)
       })
     })
     return unsubscribe;
   }, [navigation])
-  const [loading, setLoading] = useState(true)
+  const [loading, setLoading] = useState(false)
   const [artist, setArtist] = useState(sample_artist)
   const [musics, setMusics] = useState([])
-
+  const [basic_style, setBasicStyle] = useState(light_style);
 
   return (
-    <View style={[styles.container, { width: '100%', height: '100%' }]}>
+    <View style={[basic_style.container, { width: '100%', height: '100%' }]}>
       <View style={styles.header}>
         <View style={{ flexDirection: 'row' }}>
           <TouchableOpacity onPress={navigation.goBack}>
@@ -55,32 +53,32 @@ export default function ArtistScreen({ navigation, route }: StackScreenProps<Roo
             style={styles.app_logo}
             source={require('../assets/images/app_logo.png')}
           />
-          <Text style={styles.header_title}> Chords </Text>
+          <Text style={[basic_style.h1, basic_style.primary_color, basic_style.bold]}> Chords </Text>
         </View>
       </View>
       {
         loading ? <ActivityIndicator size="large" /> :
-          <View style={[styles.container, { padding: 15 }]}>
-            <Text style={styles.h1}>{artist.name}</Text>
-            <Text style={styles.h3}>{`Visitas esta semana: ${artist.season_visits}`}</Text>
-            <Text style={styles.h3}>{`Visitas totais: ${artist.total_visits}`}</Text>
+          <View style={[basic_style.container, { padding: 15 }]}>
+            <Text style={[basic_style.h1, basic_style.primary_color, basic_style.bold]}>{artist.name}</Text>
+            <Text style={[basic_style.h4, basic_style.secondary_color]}>{`Visitas esta semana: ${artist.season_visits}`}</Text>
+            <Text style={[basic_style.h4, basic_style.secondary_color]}>{`Visitas totais: ${artist.total_visits}`}</Text>
             <View style={styles.sub_header}>
-              <Text style={styles.h2}> Musicas</Text>
-              <View style={styles.separator} />
+              <Text style={[basic_style.h2, basic_style.primary_color, basic_style.bold]}> Musicas</Text>
+              <View style={basic_style.horizontal_separator} />
             </View>
-            <View style={styles.musics_container}>
+            <View>
               <ScrollView>
                 {
                   musics.map((music: MusicType, i) => {
                     return (
                       <TouchableOpacity
-                        style={styles.card}
+                        style={[basic_style.small_card, { justifyContent: 'center' }]}
                         key={i}
                         onPress={() => {
                           navigation.push('Root', { screen: 'Search', params: { screen: 'ChoseVersion', params: { music_id: music.id } } })
                         }}
                       >
-                        <Text style={styles.card_h1}>{music.name}</Text>
+                        <Text style={[basic_style.h3, basic_style.primary_color, basic_style.bold]}>{music.name}</Text>
                       </TouchableOpacity>
                     )
                   })
@@ -96,9 +94,6 @@ export default function ArtistScreen({ navigation, route }: StackScreenProps<Roo
 
 
 const styles = StyleSheet.create({
-  container: {
-    backgroundColor: '#fff',
-  },
   header: {
     backgroundColor: '#fff',
     height: 85,
@@ -119,59 +114,14 @@ const styles = StyleSheet.create({
     width: 25,
     marginRight: 20,
   },
-  header_title: {
-    color: '#333333',
-    fontSize: 18,
-    fontWeight: 'bold',
-    alignSelf: 'center',
-    fontFamily: 'raleway'
-  },
   app_logo: {
     height: 30,
     width: 30,
     marginRight: 15,
   },
-  h1: {
-    color: '#333333',
-    fontSize: 24,
-    fontFamily: 'roboto-bold',
-  },
-  h2: {
-    color: '#333333',
-    fontSize: 16,
-    fontFamily: 'roboto-bold',
-  },
-  h3: {
-    color: '#828282',
-    fontSize: 12,
-    fontFamily: 'roboto',
-  },
   sub_header: {
     marginTop: 20,
   },
-  separator: {
-    width: '100%',
-    borderBottomColor: '#E4E4E4',
-    borderBottomWidth: 1,
-    marginTop: 10,
-  },
-  musics_container: {
-    height: '100%',
-  },
-  card: {
-    backgroundColor: '#F2F2F2',
-    width: '100%',
-    height: 50,
-    borderRadius: 5,
-    justifyContent: 'center',
-    padding: 10,
-    marginTop: 10,
-  },
-  card_h1: {
-    color: '#333333',
-    fontSize: 12,
-    fontFamily: 'roboto-bold',
-  }
 });
 
 const sample_artist = {
