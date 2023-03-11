40463c71bf7bd15419c3767fb6b27c8594ccf87a
Gustavo Marques <gustavomarques.gml@gmail.com>
2023-03-11 14:42:29 -0300

    Addition of a chords in database tests

--- services/__tests__/chords.spec.ts
+++ services/__tests__/chords.spec.ts
@@ -1,4 +1,26 @@
 import { Chord } from "../chords";
+import { GuitarChordPosition } from "../../components/GuitarChord";
+
+const getAllDatabaseChords = () => {
+  const guitarChords: Record<
+    string,
+    GuitarChordPosition[]
+  > = require("../../assets/data/guitar_chords.json");
+
+  const pianoChords: Record<
+    string,
+    string[][]
+  > = require("../../assets/data/piano_chords.json");
+
+  const validChords = new Set<string>();
+  for (const chord in guitarChords) {
+    validChords.add(chord);
+  }
+  for (const chord in pianoChords) {
+    validChords.add(chord);
+  }
+  return validChords;
+};
 
 describe("Chord", () => {
   describe("Inputing valid chords", () => {
@@ -6,5 +28,15 @@ describe("Chord", () => {
       const chord = new Chord("C");
       expect(chord.base.base).toEqual(0);
     });
+  });
+
+  describe("All chords in database", () => {
+    const validChords = getAllDatabaseChords();
+    for (const chord of validChords) {
+      it(`Chord ${chord}`, () => {
+        const chordObj = new Chord(chord);
+        expect(chordObj.valid).toBeTruthy();
+      });
+    }
   });
 });
